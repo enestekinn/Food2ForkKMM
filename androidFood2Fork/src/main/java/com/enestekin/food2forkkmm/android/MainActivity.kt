6dc61c455901976ab6c7347464bdebb4bb35d7dc
Enes Tekin <>
2021-06-14 11:20:52 +0300

    defining-the-screens

--- androidFood2Fork/src/main/java/com/enestekin/food2forkkmm/android/MainActivity.kt
+++ androidFood2Fork/src/main/java/com/enestekin/food2forkkmm/android/MainActivity.kt
@@ -2,13 +2,16 @@ package com.enestekin.food2forkkmm.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
-
+import androidx.activity.compose.setContent
+import com.enestekin.food2forkkmm.android.presentation.navigation.Navigation
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            Navigation()
+        }
 
 
     }
