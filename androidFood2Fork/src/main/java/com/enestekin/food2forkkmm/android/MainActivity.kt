94a887a12c18cef925dacf4cfd25b66a27736c1b
Enes Tekin <>
2021-06-14 19:32:18 +0300

    ktor-demo

--- androidFood2Fork/src/main/java/com/enestekin/food2forkkmm/android/MainActivity.kt
+++ androidFood2Fork/src/main/java/com/enestekin/food2forkkmm/android/MainActivity.kt
@@ -4,12 +4,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import com.enestekin.food2forkkmm.android.presentation.navigation.Navigation
+import com.enestekin.food2forkkmm.datasource.network.KtorClientFactory
 import dagger.hilt.android.AndroidEntryPoint
+import io.ktor.client.request.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
+
+
+const val TOKEN = "Token 9c8b06d329136da358c2d00e76946b0111ce2c48"
+const val BASE_URL = "https://food2fork.ca/api/recipe"
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val ktorClient = KtorClientFactory().build()
+        CoroutineScope(IO).launch {
+            val recipeId = 1551
+            val recipe = ktorClient.get<String>{
+                url("$BASE_URL/get?id=$recipeId")
+                header("Authorization", TOKEN)
+            }
+            println("KtorTest: ${recipe}")
+        }
+
+
         setContent {
             Navigation()
         }
