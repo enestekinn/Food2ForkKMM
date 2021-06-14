6c6afa2796163457aaeab295ad81ef17e9156fd8
Enes Tekin <>
2021-06-14 14:29:44 +0300

    hilt-viewmodel

--- androidFood2Fork/src/main/java/com/enestekin/food2forkkmm/android/presentation/navigation/Navigation.kt
+++ androidFood2Fork/src/main/java/com/enestekin/food2forkkmm/android/presentation/navigation/Navigation.kt
@@ -5,16 +5,23 @@ import androidx.compose.material.Button
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.hilt.navigation.HiltViewModelFactory
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavType
 import androidx.navigation.compose.*
 import com.enestekin.food2forkkmm.android.presentation.recipe_detail.RecipeDetailScreen
+import com.enestekin.food2forkkmm.android.presentation.recipe_detail.RecipeDetailViewModel
 import com.enestekin.food2forkkmm.android.presentation.recipe_list.RecipeListScreen
+import com.enestekin.food2forkkmm.android.presentation.recipe_list.RecipeListViewModel
 
 @Composable
 fun Navigation() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = Screen.RecipeList.route) {
         composable(route = Screen.RecipeList.route){navBackStackEntry ->
+            val factory = HiltViewModelFactory(LocalContext.current, navBackStackEntry)
+            val viewModel: RecipeListViewModel = viewModel("RecipeListViewModel",factory)
            RecipeListScreen(onSelectRecipe = { recipeId ->
                navController.navigate(Screen.RecipeDetail.route + "/$recipeId")
            })
@@ -25,7 +32,11 @@ fun Navigation() {
                 type = NavType.IntType
             })
         ){navBackStackEntry ->
-            RecipeDetailScreen(recipeId = navBackStackEntry.arguments?.getInt("recipeId"))
+            val factory = HiltViewModelFactory(LocalContext.current,navBackStackEntry)
+            val viewModel: RecipeDetailViewModel = viewModel("RecipeDetailViewModel",factory)
+            RecipeDetailScreen(
+                recipeId = viewModel.recipeId.value
+            )
 
 
         }
---
+++ androidFood2Fork/src/main/java/com/enestekin/food2forkkmm/android/presentation/recipe_list/RecipeListViewModel.kt
@@ -0,0 +1,14 @@
+package com.enestekin.food2forkkmm.android.presentation.recipe_list
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class RecipeListViewModel
+@Inject constructor(
+    private val savedStateHandle: SavedStateHandle, // dont need for this VM
+): ViewModel(){
+
+}
