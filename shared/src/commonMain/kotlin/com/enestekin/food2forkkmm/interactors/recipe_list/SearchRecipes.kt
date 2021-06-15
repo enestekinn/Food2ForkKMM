15eb7834fe2eeba5c62b4cb59d1d999f1eeaae50
Enes Tekin <>
2021-06-15 23:47:29 +0300

    use-cases-introduction

---
+++ shared/src/commonMain/kotlin/com/enestekin/food2forkkmm/interactors/recipe_list/SearchRecipes.kt
@@ -0,0 +1,33 @@
+package com.enestekin.food2forkkmm.interactors.recipe_list
+
+import com.enestekin.food2forkkmm.datasource.network.RecipeService
+import com.enestekin.food2forkkmm.domain.model.Recipe
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class SearchRecipes(
+    private val recipeService: RecipeService,
+) {
+    fun execute(
+        page:Int,
+        query: String,
+    ): Flow<List<Recipe>> = flow {
+        //how can we emit loading?
+
+
+        //emit recipes
+        try {
+            val recipes = recipeService.search(
+                page = page,
+                query = query,
+            )
+            emit(recipes)
+
+        }catch (e: Exception){
+            //how can we emit an error
+        }
+    }
+
+
+
+}
