ad099e2962caf0ceabae258ea9d73e2c222c448c
Enes Tekin <>
2021-06-15 12:35:07 +0300

    mapping-to-and-from-core-business-model

--- shared/src/commonMain/kotlin/com/enestekin/food2forkkmm/datasource/network/KtorClientFactory.kt
+++ shared/src/commonMain/kotlin/com/enestekin/food2forkkmm/datasource/network/KtorClientFactory.kt
@@ -1,7 +1,23 @@
 package com.enestekin.food2forkkmm.datasource.network
 
+import com.enestekin.food2forkkmm.datasource.domain.model.Recipe
+import com.enestekin.food2forkkmm.datasource.network.model.RecipeDto
 import io.ktor.client.*
 
 expect class KtorClientFactory(){
     fun build(): HttpClient
+}
+
+fun RecipeDto.toRecipe(): Recipe {
+    return Recipe(
+        id = pk,
+        title = title,
+        featuredImage = featuredImage,
+        rating = rating,
+        publisher = publisher,
+        sourceUrl = sourcelUrl,
+        ingredients = ingredients,
+        dateAdded = ,//?
+        dateUpdated = ,//?
+    )
 }
