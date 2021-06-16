df714cb6fd0bb2b86e15fc029a7d9bc7b6c41a25
Enes Tekin <>
2021-06-16 16:53:37 +0300

    recipe-database-expect-actual

---
+++ shared/src/iosMain/kotlin/com/enestekin/food2forkkmm/datasource/cache/DriverFactory.kt
@@ -0,0 +1,11 @@
+package com.enestekin.food2forkkmm.datasource.cache
+
+import com.squareup.sqldelight.db.SqlDriver
+import com.squareup.sqldelight.drivers.native.NativeSqliteDriver
+
+actual class DriverFactory {
+
+    actual fun createDriver(): SqlDriver{
+        return NativeSqliteDriver(RecipeDatabase.Schema,"recipes.db")
+    }
+}
