9758627e5d276f4fdb59bce6910869a24b5c0550
Enes Tekin <>
2021-06-21 09:27:25 +0300

    horizontal selectable food

--- androidFood2Fork/src/main/java/com/enestekin/food2forkkmm/android/presentation/recipe_list/RecipeListScreen.kt
+++ androidFood2Fork/src/main/java/com/enestekin/food2forkkmm/android/presentation/recipe_list/RecipeListScreen.kt
@@ -3,10 +3,12 @@ package com.enestekin.food2forkkmm.android.presentation.recipe_list
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
 import com.enestekin.food2forkkmm.android.presentation.recipe_list.components.RecipeList
 import com.enestekin.food2forkkmm.android.presentation.recipe_list.components.SearchAppBar
 import com.enestekin.food2forkkmm.android.presentation.theme.AppTheme
+import com.enestekin.food2forkkmm.presentation.recipe_list.FoodCategoryUtil
 import com.enestekin.food2forkkmm.presentation.recipe_list.RecipeListEvents
 import com.enestekin.food2forkkmm.presentation.recipe_list.RecipeListState
 
@@ -21,10 +23,14 @@ fun RecipeListScreen(
 
     AppTheme(displayProgressBar = state.isLoading) {
 
+        val foodCategories = remember { FoodCategoryUtil().getAllFoodCategories() }
 
     Scaffold(
         topBar = {
-                 SearchAppBar(query = state.query, onQueryChange = {
+                 SearchAppBar(
+                     query = state.query,
+                     categories = foodCategories,
+                     onQueryChange = {
                  onTriggerEvent(RecipeListEvents.OnUpdateQuery(it))
                   },
                  onExecuteSearch = {
